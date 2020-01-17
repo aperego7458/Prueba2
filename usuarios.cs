ee6715f321aeea6951cd9886fc2b3f5e7c7c3be6
aperego7458 <59906585+aperego7458@users.noreply.github.com>
2020-01-17 02:56:26 -0500

    Add files via upload

---
+++ usuarios.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Prueba
+{
+    #region Usuarios
+    public class Usuarios
+    {
+        #region Member Variables
+        protected int _id;
+        protected string _nombre;
+        protected string _nickname;
+        protected int _puntuacion;
+        #endregion
+        #region Constructors
+        public Usuarios() { }
+        public Usuarios(string nombre, string nickname, int puntuacion)
+        {
+            this._nombre=nombre;
+            this._nickname=nickname;
+            this._puntuacion=puntuacion;
+        }
+        #endregion
+        #region Public Properties
+        public virtual int Id
+        {
+            get {return _id;}
+            set {_id=value;}
+        }
+        public virtual string Nombre
+        {
+            get {return _nombre;}
+            set {_nombre=value;}
+        }
+        public virtual string Nickname
+        {
+            get {return _nickname;}
+            set {_nickname=value;}
+        }
+        public virtual int Puntuacion
+        {
+            get {return _puntuacion;}
+            set {_puntuacion=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
